2e77c2bf25ad710d5877c4303da85d01e2e5df9b
L-Mariam <l.mariam.here@gmail.com>
2023-10-07 02:13:00 +0400

    removed python

--- youthify/events.py
+++
@@ -1,9 +0,0 @@
-from config import db
-
-cursor = db.cursor()
-
-def getEvents():
-    query = "SELECT title, starttime, endtime, image FROM events, event_images WHERE events.event_id = event_images.event_id;"
-    cursor.execute(query)
-    for i in cursor.fetchall():
-        yield i
